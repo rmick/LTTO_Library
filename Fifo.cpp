d788b33966eff5ca060c87fcd3e1ad79cd553487
rmick <rmick@msn.com.au>
2016-08-16 21:58:06 +1000

    Fixed issue where FIFO crashed if receiving data at boot-up
    ALWAYS initialise the variables !
    Also added a Null entry to replace the .newEntry true/false

--- Fifo.cpp
+++ Fifo.cpp
@@ -8,17 +8,19 @@
 
 void LTTO::PushToFifo(char type, uint16_t message)          // Push latest message onto FIFO
 {
-    if (_incomingIRmessageFIFO[_fifoPushPointer].processed == false)
+	if (_incomingIRmessageFIFO[_fifoPushPointer].type != ' ')
     {
         _messageOverwrittenCount++;
-        //Serial.print(F("Missed Pointer: "));
-        //Serial.print(_fifoPushPointer);
+                Serial.print(F("Missed Pointer: "));
+                Serial.print(_fifoPushPointer);
+				return;
     }
+
     _incomingIRmessageFIFO[_fifoPushPointer].type = type;
     _incomingIRmessageFIFO[_fifoPushPointer].rawDataPacket = message;
     _incomingIRmessageFIFO[_fifoPushPointer].processed = false;
     #ifdef DEBUG
-        Serial.print(F("\nPush : "));
+        Serial.print(F("\nPush:"));
         Serial.print(_fifoPushPointer);
     #endif
     _fifoPushPointer++;
@@ -28,17 +30,16 @@ void LTTO::PushToFifo(char type, uint16_t message)          // Push latest messa
 void LTTO::PopFromFifo()                                    // Pop latest message from the FIFO
 {
     // Is there a new message to collect?
-    if (_incomingIRmessageFIFO[_fifoPopPointer].processed == true)  return;
+    if (_incomingIRmessageFIFO[_fifoPopPointer].type == ' ') return;
 
-    decodedIRmessage.type = _incomingIRmessageFIFO[_fifoPopPointer].type;
-    decodedIRmessage.rawDataPacket = _incomingIRmessageFIFO[_fifoPopPointer].rawDataPacket;
+    decodedIRmessage.type			= _incomingIRmessageFIFO[_fifoPopPointer].type;
+    decodedIRmessage.rawDataPacket	= _incomingIRmessageFIFO[_fifoPopPointer].rawDataPacket;
     decodedIRmessage.newMessage = true;
-    _incomingIRmessageFIFO[_fifoPopPointer].processed = true;
-    #ifdef DEBUG
-        Serial.print(F("\t Pop : "));
+	_incomingIRmessageFIFO[_fifoPopPointer].type = ' ';		// Sets the message as empty/processed
+	#ifdef DEBUG
+        Serial.print(F("\tPop:"));
         Serial.print(_fifoPopPointer);
     #endif
-
     _fifoPopPointer++;
     if (_fifoPopPointer == FIFO_SIZE) _fifoPopPointer = 0;
 }
